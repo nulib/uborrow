64331e377b97091112c22c84ff4266779a930173
Michael Klein <mbklein@gmail.com>
2015-04-14 17:47:56 -0500

    Make the shim fully dynamic

--- uborrow.rb
+++ uborrow.rb
@@ -9,11 +9,24 @@ module Uborrow
   class Application < Sinatra::Base
     
     configure do
+      config_hash = YAML.load(File.read(File.expand_path('../config/uborrow.yml',__FILE__)))
       set :static, true
       set :public_folder, 'public'
-      set :required_fields, YAML.load(File.read(File.expand_path('../config/uborrow.yml',__FILE__)))
+      set :required_fields, config_hash['required_fields']
+      set :availability_filter, config_hash['availability_filter']
+    end
+    
+    helpers do
+      def shim
+        script_host = URI.parse(request.url).merge('/')
+        %{<script id="uborrow-shim" data-uborrow-proxy="#{script_host.to_s}" data-uborrow-filter="#{settings.availability_filter}" src="#{script_host.merge('uborrow.js').to_s}"></script>}
+      end
     end
 
+    get '/uborrow.html' do
+      shim
+    end
+    
     options '/*' do
       response_headers = {
         'Content-Type' => 'text/html; charset-utf-8',
